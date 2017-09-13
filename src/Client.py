47186864cdf583530db3814bfe6daea452a16eca
Stefan <roofus95@yandex.ru>
2017-09-13 23:18:46 +0300

    re-work server
    теперь он работает на потоках, и можно конектиться сразу нескольким клиентам., функционал сервера по-прежнему "не блещет"
    TODO  сделать массовую рассылку

--- src/Client.py
+++ src/Client.py
@@ -1,35 +1,43 @@
 import socket
 import os
-from ctypes import *
+from ctypes import windll, c_ulong
 
-
-GRAY = 8
-BLUE = 9
-GREEN = 10
-TURQUOISE = 11
-RED = 12
-PINK = 13
-YELLOW = 14
+# TODO цветную печать в отдельный модуль
+# Define color const
+GRAY = 7
+#BLUE = 9
+#GREEN = 10
+#TURQUOISE = 11
+#RED = 12
+#PINK = 13
+#YELLOW = 14
 WHITE = 15
 
+# Some stuf for using color text in consol
 windll.Kernel32.GetStdHandle.restype = c_ulong
 h = windll.Kernel32.GetStdHandle(c_ulong(0xfffffff5))
 
+
+servercol = WHITE
+
 def color_print(text, color):
     windll.Kernel32.SetConsoleTextAttribute(h, color)
-    print(text.format( color ))
-    windll.Kernel32.SetConsoleTextAttribute(h, WHITE)
+    print(text.format(color))
+    windll.Kernel32.SetConsoleTextAttribute(h, GRAY)
 
+    
 os.system('cls' if os.name == 'nt' else 'clear')
 
 sock = socket.socket()
 sock.connect(('localhost', 9090))
+nick = input("Select nickname:")
 while True:
-    str = input("User: ")
+    str = input(nick + ":")
     sock.send(bytes(str.encode('utf8')))
     data = sock.recv(1024)
-    #to_pr = ":" + str(data.upper(),'utf-8')
-    color_print(data.decode(encoding='UTF-8'), BLUE)
+    if data.decode(encoding='UTF-8') == "--exit":
+        break
+    color_print(data.decode(encoding='UTF-8'), servercol)
 sock.close()
 
-print(data)
+#TODO отдельный поток для приема данных от сервера, когда перехожит в этот поток - он захватывает управление?
--- src/Server.py
+++ src/Server.py
@@ -1,17 +1,45 @@
 import socket
+import threading
 
+class ThreadedServer():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(5)
+        print("Сервер запущен...")
+        while True:
+            client, address = self.sock.accept()
+            print("Новое подключение ", client, address)
+            client.settimeout(3600)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data 
+                    response = data
+                    client.send(response)
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    while True:
+        port_num = input("Port: ")
+        try:
+            port_num = int(port_num)
+            break
+        except ValueError:
+            pass
     
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
-
-print('connected:', addr)
-ans_list = ["Привет " + addr[0], "мммм, как интересно", "продолжай))"]
-i = 0
-while True:
-    data = conn.recv(1024)
-    ans = "Your_friend:" + ans_list[i]
-    conn.send(ans.encode('utf8'))
-    i = (i+1) % len(ans_list)
-conn.close()
+    ThreadedServer('',port_num).listen()

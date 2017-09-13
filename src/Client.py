287cde93980a0731b3a9831bcd1d567f9b1f268d
Stefan <roofus95@yandex.ru>
2017-09-13 19:23:33 +0300

    add color text in client console
    Добавлена возможность менять цвет текста вывода в консоль со стороны клиента.
    Сервер шлет сообщения из списка сообщений

--- src/Client.py
+++ src/Client.py
@@ -1,16 +1,35 @@
 import socket
 import os
+from ctypes import *
+
+
+GRAY = 8
+BLUE = 9
+GREEN = 10
+TURQUOISE = 11
+RED = 12
+PINK = 13
+YELLOW = 14
+WHITE = 15
+
+windll.Kernel32.GetStdHandle.restype = c_ulong
+h = windll.Kernel32.GetStdHandle(c_ulong(0xfffffff5))
+
+def color_print(text, color):
+    windll.Kernel32.SetConsoleTextAttribute(h, color)
+    print(text.format( color ))
+    windll.Kernel32.SetConsoleTextAttribute(h, WHITE)
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
 sock = socket.socket()
 sock.connect(('localhost', 9090))
 while True:
-    str = input()
+    str = input("User: ")
     sock.send(bytes(str.encode('utf8')))
     data = sock.recv(1024)
     #to_pr = ":" + str(data.upper(),'utf-8')
-    print(data.decode(encoding='UTF-8'))
+    color_print(data.decode(encoding='UTF-8'), BLUE)
 sock.close()
 
 print(data)
--- src/Server.py
+++ src/Server.py
@@ -1,15 +1,17 @@
 import socket
 
+    
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
 conn, addr = sock.accept()
 
 print('connected:', addr)
-
+ans_list = ["Привет " + addr[0], "мммм, как интересно", "продолжай))"]
+i = 0
 while True:
     data = conn.recv(1024)
-    ans = addr[0] + ":" + str(data.upper(),'utf-8')
+    ans = "Your_friend:" + ans_list[i]
     conn.send(ans.encode('utf8'))
-
+    i = (i+1) % len(ans_list)
 conn.close()

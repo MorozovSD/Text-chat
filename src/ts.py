2df6acb1a98fc9c15a7941d1b247e4f35b453e8e
Stefan <roofus95@yandex.ru>
2017-09-15 14:26:10 +0300

    del client_names in ts.py +fix bug
    Переделан сервер. Т.к. можно обойтись без client_names , он был убран.
    Поправленый try блоки в tc.py

--- src/ts.py
+++ src/ts.py
@@ -1,12 +1,10 @@
 import socketserver
 import sys
 class MyUDPHandler(socketserver.BaseRequestHandler):
-    # Словарь адресов пользователей, взаимодействующих с сервером
+    # Словарь адресов пользователей (keys) и их имен (value), взаимодействующих с сервером
     # по этим адресам происходит рассылка сообщений чата
     client_base  = {}   
     
-    # Словарь имен пользователей, взаимодействующих с сервером
-    client_names = {}
     
     # Отправление сообщения s, пользователям из client_base
     def mas_send(self, socket, s):
@@ -14,11 +12,10 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
             socket.sendto(bytes(s.encode('utf8')), client)
     
     # В зависимости от принимаемых данных различаются следующие типы сообщений:
-    # ::new name - проверка есть ли имя name в client_names, если имени нет, 
-    #   оповещение о появлении нового пользователя name по всем адресам client_base
-    # ::exit name - исключение name из client_names и отправка 
-    #   оповещения об этом всем адресам client_base
-    # ::members - оправка client_names запросившему пользователсю
+    # ::new name - проверка есть ли имя name в client_base.values(), если имени нет, 
+    #   оповещение о появлении нового пользователя name по всем адресам client_base.keys()
+    # ::exit name - исключение name из client_base и отправка оповещения об этом всем адресам client_base.keys()
+    # ::members - оправка информации о всех пользователях в сети client_base.values() запросившему пользователсю
     # name: "Text" - рассылка данного сообщения по всем адресам client_base
     # Если приходит запрос от пользователя, что имя не было инициализоровани в системе через
     # ::new - он отправляет сообщение ::exit. Такая ситуация возможна при перезапуске сервера
@@ -31,35 +28,36 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
         
         if not data.startswith("::"):
             name = data.split()[0][:-1]
-            if name not in self.client_names.keys():
-                self.client_names[name] = name
-                self.client_base[self.client_address] = ""
+            if name not in self.client_base.values():
+                self.client_base[self.client_address] = name
                 socket.sendto(bytes("::error_name".encode('utf8')), self.client_address)
                 
         if data.startswith("::new"):
             name = data.split()[1]
-            if name in self.client_names.keys() or name.startswith("::"):
+            if name in self.client_base.values() or name.startswith("::"):
                 socket.sendto(bytes("::no".encode('utf8')), self.client_address)
                 return
             else:
-                self.client_names[name] = name
+                self.client_base[self.client_address] = name
                 socket.sendto(bytes("::ok".encode('utf8')), self.client_address)
-                self.client_base[self.client_address] = ""
                 s = name + " присоеденился к чату!"
+                print("В сети:")
+                print(self.client_base)
                 self.mas_send(socket, s)
                 return
                 
         if data.startswith("::exit"):
-            name = data.split()[1]
+            name = self.client_base[self.client_address]
             s = name + " вышел и чата!"
             self.mas_send(socket, s)
-            self.client_names.pop(name, None)
             self.client_base.pop(self.client_address, None)
+            print("В сети:")
+            print(self.client_base)
             return
         
         if data.startswith("::members"):
             s = "В сети: \n"
-            for name in self.client_names.keys():
+            for name in self.client_base.values():
                 s += "\t" + name + "\n"
             socket.sendto(bytes(s[:-1].encode('utf8')), self.client_address)
             return
